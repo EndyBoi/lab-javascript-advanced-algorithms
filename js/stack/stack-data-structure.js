c18fedb482d72b6dab17fa401c054cf51b7e547a
Conan Browne <Ventrik101@Gmail.com>
2021-01-17 15:20:54 -0500

    Stack complete, next is DOM

--- js/stack/stack-data-structure.js
+++ js/stack/stack-data-structure.js
@@ -4,29 +4,34 @@ class StackDataStructure {
     this.MAX_SIZE = 10
   }
 
-  display() {}
-
   canPush() {
-    // ... your code goes here
+    if (this.stackControl.length === this.MAX_SIZE) return false
+    return true
   }
 
-  canPop() {}
+  display() {
+    return this.stackControl
+  }
 
   isEmpty() {
-    return !this.stackControl.length
-  }
-
-  isFull() {
-    return this.stackControl.length >= MAX_SIZE
+    if (this.stackControl.length > 0) return false
+    return true
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item)
+      return this.stackControl
+    } else {
+      return "Stack Overflow"
+    }
   }
 
   pop() {
-    if (!this.isEmpty()) {
-      // pop and lock
+    if (this.stackControl.length === 0) {
+      return "Stack Underflow"
+    } else {
+      return this.stackControl.pop()
     }
   }
 }
